b168de03ec87120fa51d1ff094e306d4e9f56a49
Ahmed-EsmaiL-web <ahmedmohamedismail2001@gmail.com>
2026-09-02 16:34:18 +0300

    Add : empty state table component and footer component

--- src/app/features/owner-page/owner-page.component.ts
+++ src/app/features/owner-page/owner-page.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
-import { ButtonModule } from 'primeng/button';
 import { HeaderComponent } from './header.component/header.component';
+import { FooterComponent } from "./footer/footer.component";
+import { TableComponent } from "./table.component/table.component";
 
 @Component({
   selector: 'app-owner-page',
   standalone: true,
-  imports: [ButtonModule , HeaderComponent],
+  imports: [HeaderComponent, FooterComponent, TableComponent],
   templateUrl: './owner-page.component.html',
   styleUrl: './owner-page.component.scss',
 })
 export class OwnerPageComponent {
-
 }
