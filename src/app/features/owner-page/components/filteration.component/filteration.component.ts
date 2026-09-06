f50e0ce6c36612c8005a97067a30d413d03097c4
Ahmed-EsmaiL-web <ahmedmohamedismail2001@gmail.com>
2026-09-06 16:29:24 +0300

    Modifed : header component , table component and search component to be shared and reusable

--- src/app/features/owner-page/components/filteration.component/filteration.component.ts
+++ src/app/features/owner-page/components/filteration.component/filteration.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+import { SharedSearchComponent } from "../../../../shared/UI/shared-search.component/shared-search.component";
 
 @Component({
   selector: 'app-filteration',
   standalone: true,
-  imports: [],
+  imports: [SharedSearchComponent],
   templateUrl: './filteration.component.html',
   styleUrl: './filteration.component.scss',
 })
 export class FilterationComponent {
-
+  
 }
--- src/app/shared/UI/shared-search.component/shared-search.component.spec.ts
+++ src/app/shared/UI/shared-search.component/shared-search.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { ButtonComponent } from './button.component';
+import { SharedSearchComponent } from './shared-search.component';
 
-describe('ButtonComponent', () => {
-  let component: ButtonComponent;
-  let fixture: ComponentFixture<ButtonComponent>;
+describe('SharedSearchComponent', () => {
+  let component: SharedSearchComponent;
+  let fixture: ComponentFixture<SharedSearchComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ButtonComponent]
+      imports: [SharedSearchComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(ButtonComponent);
+    fixture = TestBed.createComponent(SharedSearchComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
